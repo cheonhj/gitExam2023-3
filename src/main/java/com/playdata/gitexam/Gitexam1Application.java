4fb878bfa6ef1ed5afa76a0d4b1bcf893cddc2c6
hyeonji <hyunji11092@gmail.com>
2023-10-23 11:34:30 +0900

    github 테스트

---
+++ src/main/java/com/playdata/gitexam/Gitexam1Application.java
@@ -0,0 +1,13 @@
+package com.playdata.gitexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gitexam1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gitexam1Application.class, args);
+	}
+
+}
